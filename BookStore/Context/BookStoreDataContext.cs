58be333c35b46f075b114fbc339178e9113f8ccb
Queiroz <kalifor_dbel@hotmail.com>
2022-01-13 14:23:32 -0300

    Implementação de novas funcionalidades.

---
+++ BookStore/Context/BookStoreDataContext.cs
@@ -0,0 +1,21 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Context
+{
+    public class BookStoreDataContext : DbContext
+    {
+        public BookStoreDataContext()
+            :base("BookStoreConnectionString")
+        { }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+    }
+}
